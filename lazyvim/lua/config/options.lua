ddc9b38e0d7a9802a98e8f433962d50193f4eef2
Imperi13 <imperi.night@gmail.com>
2024-07-29 01:45:47 +0900

    a

--- lazyvim/lua/config/options.lua
+++ lazyvim/lua/config/options.lua
@@ -1,3 +1,12 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
+
+vim.opt.foldmethod = "syntax"
+vim.cmd([[
+augroup remember_folds
+  autocmd!
+  autocmd BufWinLeave * mkview
+  autocmd BufWinEnter * silent! loadview
+augroup END
+]])
