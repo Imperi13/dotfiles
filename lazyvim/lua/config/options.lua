8a106442a08e14f200ad07dc893dd3e346eb7ced
Imperi13 <imperi.night@gmail.com>
2025-03-24 03:00:07 +0900

    lazyvim update

--- lazyvim/lua/config/options.lua
+++ lazyvim/lua/config/options.lua
@@ -2,11 +2,14 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 -- Add any additional options here
 
-vim.opt.foldmethod = "syntax"
-vim.cmd([[
-augroup remember_folds
-  autocmd!
-  autocmd BufWinLeave * mkview
-  autocmd BufWinEnter * silent! loadview
-augroup END
-]])
+vim.opt.foldmethod = "indent"
+vim.opt.foldlevel = 99
+
+function Foldtext()
+  local line = vim.fn.getline(vim.v.foldstart)
+  local count = vim.v.foldend - vim.v.foldstart + 1
+  return string.format("%s (%d lines folded)", line, count)
+end
+
+vim.opt.foldtext = "v:lua.Foldtext()"
+vim.opt.fillchars = { fold = " " }
--- lazyvim/lua/plugins/example.lua
+++ lazyvim/lua/plugins/example.lua
@@ -1,6 +1,53 @@
 -- since this is just an example spec, don't actually load anything here and return an empty spec
 -- stylua: ignore
-if true then return {} end
+if true then return {
+    -- add gruvbox
+  { "ellisonleao/gruvbox.nvim" },
+  -- Configure LazyVim to load gruvbox
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
+
+  {
+   "Saghen/blink.cmp",
+    opts = {
+      enabled = function()
+        local filetype = vim.bo.filetype
+        local disable_cmp_filetype={
+          "copilot-chat",
+        }
+
+        if vim.tbl_contains(disable_cmp_filetype, filetype) then
+          return false
+        end
+
+        return true
+      end,
+      keymap = {
+        preset = 'default',
+        ['<S-Tab>'] = { 'select_prev', 'fallback' },
+        ['<Tab>'] = { 'select_next', 'fallback' },
+      },
+      completion = {
+        list = {
+          selection = {preselect = false,},
+        }
+      },
+    },
+  },
+
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    opts = {
+      model = 'claude-3.7-sonnet'
+    },
+  }
+  ,
+
+} end
 
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
 --
@@ -157,7 +204,11 @@ return {
     "nvim-lualine/lualine.nvim",
     event = "VeryLazy",
     opts = function(_, opts)
-      table.insert(opts.sections.lualine_x, "😄")
+      table.insert(opts.sections.lualine_x, {
+        function()
+          return "😄"
+        end,
+      })
     end,
   },
 
@@ -189,57 +240,5 @@ return {
         "flake8",
       },
     },
-  },
-
-  -- Use <tab> for completion and snippets (supertab)
-  -- first: disable default <tab> and <s-tab> behavior in LuaSnip
-  {
-    "L3MON4D3/LuaSnip",
-    keys = function()
-      return {}
-    end,
-  },
-  -- then: setup supertab in cmp
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "hrsh7th/cmp-emoji",
-    },
-    ---@param opts cmp.ConfigSchema
-    opts = function(_, opts)
-      local has_words_before = function()
-        unpack = unpack or table.unpack
-        local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-      end
-
-      local luasnip = require("luasnip")
-      local cmp = require("cmp")
-
-      opts.mapping = vim.tbl_extend("force", opts.mapping, {
-        ["<Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_next_item()
-            -- You could replace the expand_or_jumpable() calls with expand_or_locally_jumpable()
-            -- this way you will only jump inside the snippet region
-          elseif luasnip.expand_or_jumpable() then
-            luasnip.expand_or_jump()
-          elseif has_words_before() then
-            cmp.complete()
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-        ["<S-Tab>"] = cmp.mapping(function(fallback)
-          if cmp.visible() then
-            cmp.select_prev_item()
-          elseif luasnip.jumpable(-1) then
-            luasnip.jump(-1)
-          else
-            fallback()
-          end
-        end, { "i", "s" }),
-      })
-    end,
   },
 }
